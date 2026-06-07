ebfbb4d7115ca0528857da6ade7027826d6f35c7
Fernando Barraza Quintero <febaqui1008@gmail.com>
2026-06-06 18:12:41 -0700

    First commit

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,13 +3,14 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '4fqkkmt0',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
+    appId: 'dhij8qysv0a1scnwbobhpefq',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
-  }
+  },
 })
