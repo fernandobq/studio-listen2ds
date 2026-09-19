6e14655db8b8a5be20dca2b3fe2ac13978544611
Fernando Barraza Quintero <febaqui1008@gmail.com>
2026-09-18 20:10:13 -0700

    Add the schemas type list

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,3 +1,7 @@
 import {postType} from './postType'
+import {genreType} from './genreType'
+import {artistType} from './artistType'
+import {albumType} from './albumType'
+import {songType} from './songType'
 
-export const schemaTypes = [postType]
+export const schemaTypes = [postType, artistType, albumType, songType, genreType]
